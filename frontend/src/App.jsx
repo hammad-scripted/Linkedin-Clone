f5e3a1fd0c5610c1cd1c70a476ea684b24f7e0db
Mohammad Hammad <hammad.scripted@gmail.com>
2026-08-09 16:54:11 +0530

    setup tanstack-query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/layout/Layout.jsx';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
